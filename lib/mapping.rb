994e69fc79664765f50e6b2478e88d533940c181
Kem Mason <bulbous@gmail.com>
2014-10-01 13:23:53 +1300

    add default mappings for most common profanities

---
+++ lib/mapping.rb
@@ -0,0 +1,15 @@
+require 'crypt'
+
+MAPPINGS =
+{'9768634b51ca38a98892c7086c9e3122' => ['doh', 'dangit', 'arrrggghhh'],
+'3f6c82c5640cbdb903ebffb536501e1b' => ['doh', 'dangit', 'arrrggghhh'],
+'77505a312e14133e2813677b42177e12' => ['doh', 'dangit', 'arrrggghhh'],
+'de5ed538c7552a471ba1e69271af4e00' => ['doh', 'dangit', 'arrrggghhh'],
+'4a1387bafcfa3d3ebc29334e82f47d56' => ['loser', 'dork'],
+'d6c79ee62074da77e988a7bca45017d9' => ['loser', 'dork'],
+'278139c2481ec87e71a4f421bdcaa7b9' => ['loser', 'dork'],
+'cdf9360e37b1cea42eb3debfb91eec80' => ['loser', 'dork'],
+'ede96167c49f2e8906c93567aa545aeb' => ['loser', 'dork'],
+'728bb4d112e685a8375336b0a228420b' => ['loser', 'dork'],
+'f087d92fccba7cc5bf6e861dcf1d6773' => ['loser', 'dork'],
+}
